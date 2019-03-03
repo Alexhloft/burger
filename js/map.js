74c441d208e8445b562858f89d962e26f1d0b691
Khokhlov Alexander <Alexhoxlov@ya.ru>
2019-03-03 19:08:53 +0300

    week_5

---
+++ js/map.js
@@ -0,0 +1,20 @@
+ymaps.ready(init);
+function init(){ 
+    var map = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 14,
+        controls: ['zoomControl'],
+        behaviors: ['drag']
+    });
+
+    var placemark = new ymaps.Placemark([55.76, 37.64], {
+        hintContent: 'hint',
+        balloonContent: 'balloon'
+    },
+    {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/icons/map-marker.svg'
+    });
+
+    map.geoObjects.add(placemark);
+}
